b1b4df82b7599bbe14ff425283b0cc0bc3270193
mrstruijk <mrstruijk@gmail.com>
2024-10-29 12:54:04 +0100

    Starting

--- Editor/ProjectSetup.cs
+++ Editor/ProjectSetup.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.PackageManager;
@@ -7,79 +9,211 @@ using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using static System.IO.Path;
 
-/// <summary>
-/// From git-amend: https://www.youtube.com/watch?v=0_ZRHT2faQw
-/// </summary>
+
 public static class ProjectSetup
 {
-    [MenuItem("SOSXR/Setup/Import Essential Assets")]
-    private static void ImportEssentialAssets()
+    [MenuItem("SOSXR/Setup/Import Essential Editor Tools")]
+    private static void ImportEssentialEditorTools()
     {
-        ImportAssetsFrom.AssetStore("Editor Console Pro.unitypackage", "FlyingWorm/Editor ExtensionsSystem");
-        // And the rest 
-    }
-
-
-    [MenuItem("SOSXR/Setup/Install Essential Git Packages")]
-    public static void ImportEssentialGitPackages()
-    {
-        // Define only the user/repo part
-        string[] repos =
-        {
-            "solo-fsw/sosxr-unity-enhancedlogger",
-            "solo-fsw/sosxr-unity-swatchr"
-        };
-        
-        var packageUrls = ConstructGitUrls(repos);
-
-
-        ImportPackages.Packages(packageUrls);
-    }
-
-    
-    private static string[] ConstructGitUrls(string[] repos)
-    {
-        var baseUrl = "git+https://github.com/";
-        var urls = new string[repos.Length];
-
-        for (var i = 0; i < repos.Length; i++)
-        {
-            urls[i] = baseUrl + repos[i] + ".git";
-        }
-
-        return urls;
+        ImportAssetsFromJson("editor-assets");
     }
 
 
     [MenuItem("SOSXR/Setup/Install Essential Unity Packages")]
     public static void ImportEssentialUnityPackages()
     {
-        string[] packages =
-        {
-            "com.unity.inputsystem" // Needs to be last
-        };
-
-        ImportPackages.Packages(packages);
+        ImportPackagesFromJson("unity-packages");
     }
 
 
-    private static class ImportAssetsFrom
+    [MenuItem("SOSXR/Setup/Install Essential Git Packages")]
+    public static void ImportEssentialGitPackages()
     {
-        public static void AssetStore(string asset, string folder)
-        {
-            var basePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            var assetsFolder = Combine(basePath, "Unity/Asset Store-5.x");
-
-
-            AssetDatabase.ImportPackage(Combine(assetsFolder, folder, asset), false);
-        }
+        ImportPackagesFromGit("git-packages");
     }
 
 
-    public class ImportPackages
+    [MenuItem("SOSXR/Setup/CreateFolders")]
+    public static void CreateFolders()
+    {
+        Folders.Create("_SOSXR/Scripts", "_SOSXR/Textures & Materials", "_SOSXR/Models", "_SOSXR/Animation", "_SOSXR/Prefabs", "_SOSXR/Swatches", "_SOSXR/Rendering", "_SOSXR/XR", "_SOSXR/Input");
+        Folders.Move("Scenes", "_SOSXR");
+        Folders.Move("Settings", "_SOSXR");
+        Folders.Delete("TutorialInfo");
+
+        AssetDatabase.MoveAsset("Assets/InputSystem_Actions.inputactions", "Assets/_SOSXR/Input");
+        AssetDatabase.DeleteAsset("Assets/Readme.asset");
+        AssetDatabase.Refresh();
+    }
+
+
+    [MenuItem("SOSXR/Setup/Create Package Lists From Template")]
+    public static void CreateJsonFromTemplates()
+    {
+        const string targetFolder = "Assets/_SOSXR/Resources";
+
+        if (!Directory.Exists(targetFolder))
+        {
+            
+        }
+        Folders.Create(targetFolder);
+        CreateJsonFromTemplate("template-unity-packages", targetFolder, "unity-packages.json");
+        CreateJsonFromTemplate("template-editor-assets", targetFolder, "editor-assets.json");
+        CreateJsonFromTemplate("template-git-packages", targetFolder, "git-packages.json");
+    }
+
+
+    private static void ImportAssetsFromJson(string fileName)
+    {
+        var file = Resources.Load<TextAsset>(fileName);
+
+        if (file == null)
+        {
+            Debug.LogError($"{fileName}.json not found in Resources folder.");
+
+            return;
+        }
+
+        var data = JsonUtility.FromJson<AssetList>(file.text);
+
+        if (data?.assets == null || data.assets.Length == 0)
+        {
+            Debug.LogError($"Nothing found in {fileName}.json.");
+
+            return;
+        }
+
+        Import.FromAssetStore(data.assets);
+    }
+
+
+    private static void ImportPackagesFromJson(string fileName)
+    {
+        var file = Resources.Load<TextAsset>(fileName);
+
+        if (file == null)
+        {
+            Debug.LogError($"{fileName}.json not found in Resources folder.");
+
+            return;
+        }
+
+        var data = JsonUtility.FromJson<PackageList>(file.text);
+
+        if (data?.packages == null || data.packages.Length == 0)
+        {
+            Debug.LogError($"Nothing found in {fileName}.json.");
+
+            return;
+        }
+
+        Import.Packages(data.packages);
+    }
+
+
+    private static void ImportPackagesFromGit(string fileName)
+    {
+        var file = Resources.Load<TextAsset>(fileName);
+
+        if (file == null)
+        {
+            Debug.LogError($"{fileName}.json not found in Resources folder.");
+
+            return;
+        }
+
+        var data = JsonUtility.FromJson<PackageList>(file.text);
+
+        if (data?.packages == null || data.packages.Length == 0)
+        {
+            Debug.LogError($"Nothing found in {fileName}.json.");
+
+            return;
+        }
+
+        Import.Packages(ConstructGitUrls(data.packages));
+    }
+
+
+    private static string[] ConstructGitUrls(string[] repos)
+    {
+        return repos.Select(repo => $"https://github.com/{repo}.git").ToArray();
+    }
+
+
+    private static void CreateJsonFromTemplate(string templateName, string targetFolder, string newFileName)
+    {
+        var template = Resources.Load<TextAsset>(templateName);
+
+        if (template == null)
+        {
+            Debug.LogError($"Template '{templateName}' not found in Resources folder.");
+
+            return;
+        }
+
+        var targetPath = Combine(Application.dataPath, targetFolder);
+        Folders.Create(targetPath);
+
+        File.WriteAllText(Combine(targetPath, newFileName), template.text);
+        Debug.Log($"Created JSON file '{newFileName}' at '{targetFolder}' using template '{templateName}'.");
+        AssetDatabase.Refresh();
+    }
+
+
+    [Serializable]
+    private class PackageList
+    {
+        public string[] packages;
+    }
+
+
+    [Serializable]
+    private class AssetList
+    {
+        public string[] assets;
+    }
+
+
+    private static class Import
     {
         private static AddRequest _request;
         private static readonly Queue<string> PackagesToInstall = new();
+
+
+        public static void FromAssetStore(string[] assets)
+        {
+            Array.ForEach(assets, FromAssetStore);
+        }
+
+
+        private static void FromAssetStore(string path)
+        {
+            var assetsFolder = Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Unity/Asset Store-5.x");
+
+            if (!Directory.Exists(assetsFolder))
+            {
+                Debug.LogWarning($"Folder not found: {assetsFolder}. Please download asset '{path.Split("/")[^1]}' from Asset Store.");
+
+                return;
+            }
+
+            AssetDatabase.ImportPackage(Combine(assetsFolder, path), false);
+        }
+
+
+        public static void Packages(string[] packages)
+        {
+            foreach (var package in packages)
+            {
+                PackagesToInstall.Enqueue(package);
+            }
+
+            if (PackagesToInstall.Count > 0)
+            {
+                StartNextPackageInstallation();
+            }
+        }
 
 
         private static async void StartNextPackageInstallation()
@@ -93,9 +227,8 @@ public static class ProjectSetup
 
             if (_request.Status == StatusCode.Success)
             {
-                Debug.Log("Installed: " + _request.Result.packageId);
+                Debug.Log($"Installed: {_request.Result.packageId}");
             }
-
             else if (_request.Status >= StatusCode.Failure)
             {
                 Debug.LogError(_request.Error.message);
@@ -107,24 +240,69 @@ public static class ProjectSetup
                 StartNextPackageInstallation();
             }
         }
-
-
-        public static void Packages(string[] packages)
-        { 
-            foreach (var package in packages)
-            {
-                PackagesToInstall.Enqueue(package);
-            }
-
-            if (PackagesToInstall.Count > 0)
-            {
-                StartNextPackageInstallation();
-            }
-        }
     }
 
 
-    public class Folders
+    private static class Folders
     {
+        public static void Create(string root, params string[] folders)
+        {
+            var fullPath = Combine(Application.dataPath, root);
+
+            if (!Directory.Exists(fullPath))
+            {
+                Directory.CreateDirectory(fullPath);
+            }
+
+            foreach (var folder in folders)
+            {
+                CreateSubFolders(fullPath, folder);
+            }
+        }
+
+
+        private static void CreateSubFolders(string rootPath, string folderHierarchy)
+        {
+            var currentPath = rootPath;
+
+            foreach (var folder in folderHierarchy.Split('/'))
+            {
+                currentPath = Combine(currentPath, folder);
+
+                if (!Directory.Exists(currentPath))
+                {
+                    Directory.CreateDirectory(currentPath);
+                }
+            }
+        }
+
+
+        public static void Move(string folderName, string destination)
+        {
+            var sourcePath = $"Assets/{folderName}";
+
+            if (!AssetDatabase.IsValidFolder(sourcePath))
+            {
+                return;
+            }
+
+            var error = AssetDatabase.MoveAsset(sourcePath, $"Assets/{destination}/{folderName}");
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                Debug.LogWarning($"Failed to move {folderName}: {error}");
+            }
+        }
+
+
+        public static void Delete(string folderName)
+        {
+            var pathToDelete = $"Assets/{folderName}";
+
+            if (AssetDatabase.IsValidFolder(pathToDelete))
+            {
+                AssetDatabase.DeleteAsset(pathToDelete);
+            }
+        }
     }
 }
